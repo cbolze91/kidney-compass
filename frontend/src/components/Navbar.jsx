29fb1aaf59b590498a29dab226e6934f1f41231a
Christina <christinamarieelaine@gmail.com>
2026-07-06 15:29:45 -0700

    Add sign out functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,48 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
+
+  const token = localStorage.getItem('token');
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/');
+  };
+
   return (
     <nav className="navbar">
-      <Link to="/" className="logo">Kidney Compass</Link>
+      <Link to="/" className="logo">
+        Kidney Compass
+      </Link>
 
       <div className="nav-links">
         <Link to="/resources">Trusted Resources</Link>
+
         <Link to="/topics">Learning Library</Link>
-        <Link to="/questions">My Questions</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/signup" className="nav-button">Sign Up</Link>
+
+        {token && (
+          <>
+            <Link to="/questions">My Questions</Link>
+
+            <button
+              onClick={handleLogout}
+              className="nav-button"
+            >
+              Sign Out
+            </button>
+          </>
+        )}
+
+        {!token && (
+          <>
+            <Link to="/login">Login</Link>
+
+            <Link to="/signup" className="nav-button">
+              Sign Up
+            </Link>
+          </>
+        )}
       </div>
     </nav>
   );
